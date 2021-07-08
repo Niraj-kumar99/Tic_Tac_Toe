0a939fe179fa71f21a0a270406007427a8e6f14f
Niraj-kumar99 <kumarnkj35@gmail.com>
2021-07-08 09:41:01 +0530

    Refactored board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,16 +1,23 @@
 
 public class TicTacToeGame {
-	public static void main(String[] args) {
-		
+	
+	public static void board()
+	{
 		char[] board = new char[10];
 		
-		for (int i = 0 ; i<board.length ; i++)
+		for (int i = 1 ; i<board.length ; i++)
 		{
 			board[i] = ' ';
 			
-			System.out.println(board);
 		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the game . ");
+		board();
+		
 		
 	}
 
 }
+
