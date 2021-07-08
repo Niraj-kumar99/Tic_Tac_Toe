c2055077e073145420d083e023264d573af10293
Niraj-kumar99 <kumarnkj35@gmail.com>
2021-07-08 10:23:51 +0530

    Refactored code with method name

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,20 +1,19 @@
 
 public class TicTacToeGame {
 	
-	public static void board()
+	// board method declaration ... 
+	public static void createBoard()
 	{
 		char[] board = new char[10];
-		
 		for (int i = 1 ; i<board.length ; i++)
 		{
 			board[i] = ' ';
-			
 		}
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the game . ");
-		board();
+		createBoard();  // Method calling ..
 		
 		
 	}
